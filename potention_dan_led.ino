f54da8dc1714d2b6f598d651137f9bf29865908e
zoggi02 <zoggi02@gmail.com>
2017-11-17 08:49:31 +0700

    penambahan fungsi

--- potention_dan_led.ino
+++ potention_dan_led.ino
@@ -10,7 +10,14 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  baca();
+  digitalWrite(ledPin, HIGH); //menyalakan ledPin
+  delay(value1);               //memberi waktu jeda sebanyak nilai potentiometer
+  digitalWrite(ledPin, LOW);  //mematikan ledPin
+  delay(value2);               //memberi waktu jeda sebanyak nilai potentiometer
+}
+
+void baca(){
   value1 = analogRead(pinPotentiometer1); //membuat variabel value membaca nilai dari pin analog
   value2 = analogRead(pinPotentiometer2); //membuat variabel value membaca nilai dari pin analog
   Serial.println(value1);
@@ -34,10 +41,4 @@ void loop() {
     value2 = 5000;
   }
   
-  digitalWrite(ledPin, HIGH); //menyalakan ledPin
-  delay(value1);               //memberi waktu jeda sebanyak nilai potentiometer
-  digitalWrite(ledPin, LOW);  //mematikan ledPin
-  delay(value2);               //memberi waktu jeda sebanyak nilai potentiometer
 }
-
-//source http://www.kelasrobot.com/2015/10/program-arduino-analog-input-potensiometer.html
